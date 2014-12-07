bebd0106c7ba4b30f78336f829b990ed1be1ca59
davidtam28 <nest1e@hotmail.com>
2014-12-07 20:04:02 +0000

    Change solution to use plot instead of ggplot

--- plot3.R
+++ plot3.R
@@ -12,27 +12,11 @@ dataf$V9 <- as.numeric(as.character(dataf$V9))
 png(filename = "plot3.png", width = 480, height = 480)
 
 
-df <- data.frame( time = dataf$V2, Sub_metering_1 = dataf$V7,
-                 Sub_metering_2 = dataf$V8,
-                 Sub_metering_3 = dataf$V9)
+plot(dataf$V2, dataf$V7, type="l", xlab="", ylab="Energy Sub metering", col="black")
+lines(dataf$V2, dataf$V8, type="l", xlab="", col="red")
+lines(dataf$V2, dataf$V9, type="l", xlab="", col="blue")
+legend("topright", legend = c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"),
+       col = c("black", "red", "blue"), lwd=1)
 
-df <- melt(df ,  id = 'time', variable_name = 'series')
-
-plot3 <- ggplot(df, aes(time,value)) + geom_line(aes(colour = series))
-plot3 <- plot3 + scale_color_manual(values=c("black", "red", "blue"))
-plot3 <- plot3 + theme(panel.background = element_rect(fill = 'white', colour = 'BLACK'))
-plot3 <- plot3 + theme(
-                  plot.background = element_blank()
-                  ,panel.grid.major = element_blank()
-                  ,panel.grid.minor = element_blank()
-                  ,panel.border = element_blank()
-                  ,legend.position = c(1,1)
-                  , legend.justification = c(1,1)
-                )
-plot3 <- plot3 + theme(legend.title=element_blank())
-plot3 <- plot3 + theme(axis.title.x=element_blank())
-plot3 <- plot3 + ylab("Energy sub metering")
-plot3 <- plot3 + scale_x_datetime(labels = date_format("%a"), breaks = date_breaks("day"))
-plot3
 dev.off()
 
